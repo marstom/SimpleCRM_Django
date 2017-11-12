49ced8750e6a64db4fac756e45ca422e9e9cc5e7
marstom <marstomek@gmail.com>
2017-11-12 13:20:47 +0000

    Before megre

--- mycrm/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from mycrm.models import Company, BusinessCard, Order
-
-# Register your models here.
-
-admin.site.register(Company)
-admin.site.register(BusinessCard)
-admin.site.register(Order)
