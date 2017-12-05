0160bf90a9d503e45b339027d1feac5da68d5b80
Tomasz Marszałek <tomasz.marszalek@idego.pl>
2017-12-05 11:14:37 +0100

    new login form

--- SimpleCRM/settings.py
+++ SimpleCRM/settings.py
@@ -135,6 +135,9 @@ MESSAGE_TAGS = {
     messages.ERROR: 'alert-danger',
 }
 
+#after login
+LOGIN_REDIRECT_URL = '/'
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
