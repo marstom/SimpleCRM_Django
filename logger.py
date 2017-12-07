09d7f3593b3ca2c91182709ed8e8af97c2ae6778
Tomasz Marszałek <tomasz.marszalek@idego.pl>
2017-12-07 12:30:55 +0100

    Activate logger ... add templates folder, ready to merge with server

--- logger.py
+++ logger.py
@@ -4,14 +4,14 @@ logging events
 import logging
 
 logger = logging.getLogger(__name__)
-#
-# logging.basicConfig(level=logging.DEBUG,
-#                     filename='logi.log',
-#                     format='%(asctime)s %(relativeCreated)s %(name)s %(levelname)s >>> %(message)s'
-#                     )
 
-
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.DEBUG,
+                    filename='./logi-mycrm.log',
                     format='%(asctime)s %(relativeCreated)s %(name)s %(levelname)s >>> %(message)s'
                     )
+
+
+# logging.basicConfig(level=logging.INFO,
+#                     format='%(asctime)s %(relativeCreated)s %(name)s %(levelname)s >>> %(message)s'
+#                     )
 
