d6842116aca9d4ce1ea77bb63d5a8c4b3a8f7723
Tomasz Marszałek <tomasz.marszalek@idego.pl>
2017-12-11 16:08:00 +0100

    * detail page has tabs in jquery * page is available in local network for all hosts

--- SimpleCRM/settings.py
+++ SimpleCRM/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = local.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 INTERNAL_IPS = ('127.0.0.1',)
 
 
